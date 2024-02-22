54a267f6fb4b83063778bc23eb038263babcfb32
Dan4kpromax28 <danikbalik@gmail.com>
2024-02-22 12:10:13 +0200

    Some code

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -1,2 +1,59 @@
-package model;public class Professor {
+package model;
+
+
+public class Professor {
+    //1.variables
+    private long pId;
+    private String name;
+    private String surname;
+    private Degree degree;
+
+    private static long counter = 0; // ja to maino kkur tas mainas visur
+
+
+    //2.get and set
+
+    public long getpId() {
+        return pId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Degree getDegree() {
+        return degree;
+    }
+
+    public void setpId() {
+        this.pId = counter;
+        counter++;
+    }
+
+    public void setName(String name) {
+        if (name != null && name.matches("[A-Z]{1}[a-z]+")){
+            this.name = name;
+        }
+        else {
+            
+        }
+
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setDegree(Degree degree) {
+        this.degree = degree;
+    }
+    //3.constructor
+
+    //4.toString function
+    //5.other function
+
 }
